77d398d832c689777f17a8482966c1e7d8c8d851
Mani <m.tahriri.z@gmail.com>
2019-12-04 22:21:53 +0100

    conditions and swith-case is done

---
+++ conditionals.php
@@ -0,0 +1,30 @@
+<?php
+    $dayofweek = date("w");
+
+    switch ($dayofweek){
+        case 1:
+            echo "It is Monday";
+        break;
+        case 2:
+            echo "It is Tuesday";
+        break;
+        case 3:
+            echo "It is Wendsday";
+        break;
+        case 4:
+            echo "It is Thursday";
+        break;
+        case 5:
+            echo "It is Friday";
+        break;
+        case 6:
+            echo "It is Saturday";
+        break;
+        case 0:
+            echo "It is Sunday";
+        break;
+
+    }
+
+
+?>
