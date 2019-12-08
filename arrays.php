6d20b6ad9b94c476894ca1f0bce4d2fa5232c085
Mani <m.tahriri.z@gmail.com>
2019-12-08 01:26:34 +0100

    arrays created

--- arrays.php
+++ arrays.php
@@ -1,5 +1,5 @@
 <?php
-    /* there are 3 types of Array
+/* there are 3 types of Array
         - Indexed
         - Associative
         - Multidimensional
@@ -16,6 +16,28 @@
     $caes[] = "Aston Martin";
     // handy tools to see what is inside an array
     var_dump($cars);
+    echo "<br>";
+    //Associative Array --- key => value
+    $class = array("Brayan" => 25 , "Jennifer" => 28 , "ALex" => 22);
+    print_r($class);
+    // Multidimensional Array
+    $carshop = array(
+        array("Honda", 20 , 10),
+        array("BMW", 30 , 11),
+        array("Ferrari" , 1 , 2)
+    );
+    echo "<br>" . $carshop[1][2];
+    // we want to find out how many memebr we have in people array
+    echo "<br> we have " . count($people) . " members in our arrayr <br>";
+    // we want to combine [numbers] as key and [people] as value to new array
+    $combined = array_combine($numbers , $people);
+    var_dump($combined);
 
-    
-?>
+    $lastppl = end($people);
+    print "<br>The last element of people array is " . $lastppl . "<br>";
+
+    // add new element to the end of array
+    array_push($cars , "Kia Motors");
+    var_dump($cars);
+
+    ?>
