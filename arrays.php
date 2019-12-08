b9563d4971785534f05485227272b5ff40db9c0d
Mani <m.tahriri.z@gmail.com>
2019-12-08 01:19:54 +0100

    all arrays defined

---
+++ arrays.php
@@ -0,0 +1,21 @@
+<?php
+    /* there are 3 types of Array
+        - Indexed
+        - Associative
+        - Multidimensional
+    */ 
+
+    // Indexed String Array
+    $people = array('John', 'Sam', 'Nicole');
+
+    $numbers = array(11 , 5.2 , 586.25 , 65 );
+
+    $cars = ['BMW', 'Toyota' , 'Nissan'];
+    $cars[4] = "Mercedes";
+    // add new index to the end of array
+    $caes[] = "Aston Martin";
+    // handy tools to see what is inside an array
+    var_dump($cars);
+
+    
+?>
